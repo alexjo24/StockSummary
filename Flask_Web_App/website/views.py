f299f3a011b47698a53f8454d5b7ffbb12eba617
alexjo24 <alebale2@gmail.com>
2021-03-17 15:09:03 +0100

    Update views.py
    Added a better description.

--- Flask_Web_App/website/views.py
+++ Flask_Web_App/website/views.py
@@ -31,8 +31,11 @@ def hot():
 def dd():
 
     DD_Stocks = DDStocks.query.limit(20)
-    ls_vis_Stocks,ls_vis_values,date_list = dbToList() #Alters the database table data for visualization purposes. A dictionary with all the top ten stocks and its stock mentions for the last week is included. A list of the respective dates is included as well.
-
+    ls_vis_Stocks, ls_vis_values, date_list = dbToList()    #Exports the data from the database table data restructure it for visualization purposes.
+                                                            #Three lists are output from the method:
+                                                            # ls_vis_Stocks: the top ten stocks symbols. (String)
+                                                            # ls_vis_value: the stock symbol respective stock mentions for the last week. (Int)
+                                                            # date_list: respective dates in the order which matches the when a stock mention was webscraped. (String)
     return render_template("dd_section.html", DD_Stocks=DD_Stocks, user=current_user, ls_vis_Stocks=ls_vis_Stocks, ls_vis_values=ls_vis_values, date_list=date_list)
 
 #Convert database table data to lists. This is to enable visualization with chart.js.
