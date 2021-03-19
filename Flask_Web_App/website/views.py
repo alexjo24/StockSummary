43c36fa3b52af28ec76467b84d6ae7e5c443593f
alexjo24 <alebale2@gmail.com>
2021-03-19 16:00:22 +0100

    Update views.py
    Fixed chart.js visualization. Before the fix the top 10 most mentioned stocks were not the ones represented in the chart.

--- Flask_Web_App/website/views.py
+++ Flask_Web_App/website/views.py
@@ -86,8 +86,36 @@ def dbToList():
     ls_vis_Stocks = list(dict_mentions.keys())
     ls_vis_values = list(dict_mentions.values())
 
+    print("YO: ",ls_vis_Stocks)
+    print("LO: ", ls_vis_values)
+    ls_vis_Stocks,ls_vis_values = sortStockLists(ls_vis_Stocks,ls_vis_values)
+    # sortStockLists(ls_vis_Stocks,ls_vis_values)
 
     return ls_vis_Stocks,ls_vis_values, date_list
+
+
+
+
+#Stock lists used for visualization in chart.js is sorted by todays most mentioned stock in descending order.
+def sortStockLists(stock_symbols,values):
+
+
+    for i in range(len(values)):
+        values[i].append(stock_symbols[i])
+
+    print("lo:", values[:2])
+    
+    values.sort(key=lambda x: x[-2], reverse=True)
+    print("yo:", values[:2])
+
+    stock_symbols = []
+    for j in range(len(values)):
+        stock_symbols.append(values[j][-1])
+        del values[j][-1]
+
+    print("yo:", values[:2])
+    print("stonks:", stock_symbols)
+    return stock_symbols,values
 
 #Get a list of all stock symbols that has been on the top ten latest week.
 def getStockSymbols(dd_list):
